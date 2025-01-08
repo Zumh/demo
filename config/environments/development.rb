e6324568c3b42d42b83f7fca6d02f78fd64dc800
Zumh <17211423+Zumh@users.noreply.github.com>
2025-01-08 16:38:37 -0500

    demo dynamic

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # 
+  # Configure Web Console to allow access from any IP address
+  config.web_console.whitelisted_ips = %w[ 0.0.0.0/0 ::/0 ]
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
